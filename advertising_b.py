520320121311e304766a4d1827b1da136878ad3d
hakmajohari <49939551+hakmajohari@users.noreply.github.com>
2023-12-14 14:14:33 +0800

    Update advertising_b.py

--- advertising_b.py
+++ advertising_b.py
@@ -11,18 +11,16 @@ This app predicts the **Advertiding channel** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_advertising():
-    tv = st.sidebar.slider('TV ', 4.3, 7.9, 5.4)
-    radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
-    newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
-    sales = st.sidebar.slider('Sales', 0.1, 2.5, 0.2)
-    data = {'tv': tv,
-            'sradio': radio,
-            'newspaper': newspaper,
-            'sales': sales}
-    advertising = pd.DataFrame(data, index=[0])
-    return advertising
+        tv = st.sidebar.slider('TV ', 4.3, 7.9, 5.4)
+        radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
+        newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+        data = {'tv': tv,
+                'sradio': radio,
+                'newspaper': newspaper}
+        sale = pd.DataFrame(data, index=[0])
+        return sale
 
-df = user_input_advertising()
+df = user_input_sale()
 
 st.subheader('User Input parameters')
 st.write(df)
