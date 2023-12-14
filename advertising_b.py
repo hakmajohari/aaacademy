3e0cf98e56a58396252bbf9f122549e76aa212fe
hakmajohari <49939551+hakmajohari@users.noreply.github.com>
2023-12-14 14:28:43 +0800

    Update advertising_b.py

--- advertising_b.py
+++ advertising_b.py
@@ -11,9 +11,9 @@ This app predicts the **Advertiding channel** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_advertising():
-        tv = st.sidebar.slider('TV ', 4.3, 7.9, 5.4)
-        radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
-        newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
+        tv = st.sidebar.slider('TV ', 0.0, 25.0, 5.4)
+        radio = st.sidebar.slider('Radio', 0.0, 25.0, 3.4)
+        newspaper = st.sidebar.slider('Newspaper', 0.0, 25.0, 1.3)
         data = {'tv': tv,
                 'sradio': radio,
                 'newspaper': newspaper}
