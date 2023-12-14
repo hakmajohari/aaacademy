9abb19a1984a9add780e3e76c78926061d980399
hakmajohari <49939551+hakmajohari@users.noreply.github.com>
2023-12-14 14:25:22 +0800

    Update advertising_ml.py

--- advertising_ml.py
+++ advertising_ml.py
@@ -14,11 +14,11 @@ def user_input_advertising():
     tv = st.sidebar.slider('TV ', 4.3, 7.9, 5.4)
     radio = st.sidebar.slider('Radio', 2.0, 4.4, 3.4)
     newspaper = st.sidebar.slider('Newspaper', 1.0, 6.9, 1.3)
-    sales = st.sidebar.slider('Sales', 0.1, 2.5, 0.2)
+    # sales = st.sidebar.slider('Sales', 0.1, 2.5, 0.2)
     data = {'tv': tv,
             'sradio': radio,
-            'newspaper': newspaper,
-            'sales': sales}
+            'newspaper': newspaper}
+            # 'sales': sales}
     advertising = pd.DataFrame(data, index=[0])
     return advertising
 
