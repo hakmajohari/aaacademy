15753ddc37b15bc335b0a4b143ae51c4ea715e1c
hakmajohari <49939551+hakmajohari@users.noreply.github.com>
2023-12-14 14:35:00 +0800

    Update advertisingnew.py

--- advertisingnew.py
+++ advertisingnew.py
@@ -14,9 +14,9 @@ def user_input_advertising():
         tv = st.sidebar.slider('TV ', 0.0, 25.0, 5.4)
         radio = st.sidebar.slider('Radio', 0.0, 25.0, 3.4)
         newspaper = st.sidebar.slider('Newspaper', 0.0, 25.0, 1.3)
-        data = {'tv': tv,
-                'sradio': radio,
-                'newspaper': newspaper}
+        data = {'TV': tv,
+                'Radio': radio,
+                'Newspaper': newspaper}
         sale = pd.DataFrame(data, index=[0])
         return sale
 
